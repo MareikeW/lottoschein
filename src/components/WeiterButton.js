721691a399582fb4ab5f703e08e1494d193c01df
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-05-19 10:57:00 +0200

    Styling der ausgewählten Zahlen + deren Sortierung

--- src/components/WeiterButton.js
+++ src/components/WeiterButton.js
@@ -10,12 +10,24 @@ const WeiterButton = () => {
             setFinalSelectedFelder(prevState => [...prevState, selectedFeld.id]);
         });
     }
+
+    /* Arraydestrukturierung mit compare-function, um die Zahlen zu sortieren */
+    let [zahl1, zahl2, zahl3, zahl4, zahl5, zahl6] = finalSelectedFelder.sort((a, b) => a - b);
       
     return (
         <div>
             {finalSelectedFelder.length < 6 
                 ? <button className="weiterButton" onClick={handleSubmit}>Weiter</button> 
-                : <div className="outputAnzeige">Ihre Zahlen: <br/> { finalSelectedFelder.join(" ") }</div>
+                : <div className="outputAnzeige">Ihre Zahlen: 
+                    <ul id="finalZahlenContainer">
+                        <li class="ziffer">{zahl1}</li>
+                        <li class="ziffer">{zahl2}</li>
+                        <li class="ziffer">{zahl3}</li>
+                        <li class="ziffer">{zahl4}</li>
+                        <li class="ziffer">{zahl5}</li>
+                        <li class="ziffer">{zahl6}</li>
+                    </ul>
+                </div>
             }
         </div>
     )
